a2f5f315b99e2b7b9c18b96aa34c3da63fb0e511
Brazile Jason <netceteragroupadmin@example.com>
2013-01-16 17:48:57 +0100

    closer to a good solution

--- beam-3dveglab-vlab/src/main/resources/auxdata/replaceWithLatest.bat
+++ beam-3dveglab-vlab/src/main/resources/auxdata/replaceWithLatest.bat
@@ -1,33 +1,52 @@
 @echo off
 setlocal
-rem data is the only place I could write to
-set beaminstpath="C:\data\Program Files (x86)\beam-4.10.3"
-echo %beaminstpath%
-if not exist %beaminstpath% (
-  if exist "%ProgramFiles(x86)%\beam-4.10.3" (
-    set beaminstpath "%ProgramFiles(x86)%\beam-4.10.3"
-  ) else (
-    echo "%ProgramFiles(x86)%\beam-4.10.3" did not exist
-    if exist "%ProgramFiles%\beam-4.10.3" (
-      set beaminstpath "%ProgramFiles%\beam-4.10.3"
-    ) else (
-      echo "%ProgramFiles%\beam-4.10.3" did not exist
-      echo Can't find BEAM installation path
-      goto :eof
-    )
-  )
-)
-set beamdata=%HOMEDRIVE%%HOMEPATH%\.beam\
+
+cd /d %HOMEDRIVE%
+echo Searching %HOMEDRIVE% for beam installation...
+call :findbeam
+if "%beaminstpath%" == "" (
+  echo Error: beam installation not found on %HOMEDRIVE%
+  echo Failed
+  exit /b 1
+) 
+
+echo beaminstpath is "%beaminstpath%"
+
+echo "Running beam to unpack 3dvlab auxdata..."
+"%beaminstpath%\jre\bin\java.exe" ^
+    -Xmx1024M ^
+    -Dceres.context=beam ^
+    "-Dbeam.mainClass=org.esa.beam.framework.processor.ProcessorRunner" ^
+    "-Dbeam.processorClass=com.netcetera.vlab.VLabProcessor" ^
+    "-Dbeam.home=%beaminstpath%" ^
+    "-Dncsa.hdf.hdflib.HDFLibrary.hdflib=%beaminstpath%\modules\lib-hdf-2.7\lib\jhdf.dll" ^
+    "-Dncsa.hdf.hdf5lib.H5.hdf5lib=%beaminstpath%\modules\lib-hdf-2.7\lib\jhdf5.dll" ^
+    -jar "%beaminstpath%\bin\ceres-launcher.jar" h:\dummy.xml
+
 set helper=%HOMEDRIVE%%HOMEPATH%\.beam\beam-vlab\auxdata\doshelper.jar
 if not exist %helper% (
-  echo .beam\beam-vlab\ausdata\doshlper.jar wasn't found
-  goto :eof
+  echo %HOMEDRIVE%%HOMEPATH%\.beam\beam-vlab\auxdata\doshelper.jar not found
+  exit /b 1
 )
-cd "%beamdata%"
-mkdir beam-vlab.new
-cd beam-vlab.new
-mkdir auxdata
-cd auxdata
-%beaminstpath%\jre\bin\java -jar %helper% fetch dummy_windows32-20130103.zip
-%beaminstpath%\jre\bin\java -jar %helper% unzip dummy_windows32-20130103.zip
+
+echo "Successfully unpacked 3dvlab auxdata"
+cd /d %HOMEDRIVE%%HOMEPATH%\.beam\beam-vlab\auxdata
+
+rd /q /s dummy_windows32*.*
+"%beaminstpath%"\jre\bin\java -jar %helper%  fetch ftp://ftp.netcetera.ch/pub/dummy_windows32-20130103.zip
+"%beaminstpath%"\jre\bin\java -jar %helper%  unzip dummy_windows32-20130103.zip
+>> Versions.txt echo dummy_windows32-20130103.zip
+rename dummy_windows32-20130103 dummy_windows32
+del dummy_windows32-20130103.zip
+goto :done
+
+:findbeam
+for /f "usebackq tokens=*" %%a in (`dir /s/b \*beam-4.10.3`) do (
+  if exist %%a set beaminstpath=%%a
+  exit /b
+)
+
+:done
+echo Successfully completed
 endlocal
+exit /b 0
