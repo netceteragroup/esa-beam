c97fb0a52edbc52a6cadcdbf29a28586ee00557a
Brazile Jason <netceteragroupadmin@example.com>
2013-01-17 17:45:47 +0100

    this works better

--- beam-3dveglab-vlab/src/main/resources/auxdata/replaceWithLatest.bat
+++ beam-3dveglab-vlab/src/main/resources/auxdata/replaceWithLatest.bat
@@ -4,15 +4,34 @@ setlocal
 cd /d %HOMEDRIVE%
 echo Searching %HOMEDRIVE% for beam installation...
 call :findbeam
+
 if "%beaminstpath%" == "" (
   echo Error: beam installation not found on %HOMEDRIVE%
   echo Failed
   exit /b 1
 ) 
 
-echo beaminstpath is "%beaminstpath%"
+echo Using beaminstpath = "%beaminstpath%"
 
-echo "Running beam to unpack 3dvlab auxdata..."
+echo Creating 3dveglab dummy input file: %TEMP%\dummy.xml
+
+ > %TEMP%\dummy.xml echo ^<?xml version="1.0" encoding="ISO-8859-1"?^>
+>> %TEMP%\dummy.xml echo ^<RequestList^>
+>> %TEMP%\dummy.xml echo    ^<Request type="VLAB"^>
+>> %TEMP%\dummy.xml echo        ^<Parameter name="3dScene" value="Default RAMI" /^>
+>> %TEMP%\dummy.xml echo        ^<OutputProduct file="vlab_out.dim" format="BEAM-DIMAP" /^>
+>> %TEMP%\dummy.xml echo    ^</Request^>
+>> %TEMP%\dummy.xml echo ^</RequestList^>
+
+if exist %HOMEDRIVE%%HOMEPATH%\.beam\beam-vlab (
+  echo Removing existing beam-vlab...
+  echo PRESS CONTROL-C WITHIN 10 SECONDS TO CANCEL!
+  ping 1.1.1.1 -n 1 -w 10000 > nul 2>&1
+  rd /q /s %HOMEDRIVE%%HOMEPATH%\.beam\beam-vlab 
+)
+
+echo Running beam to unpack 3dveglab auxdata...
+
 "%beaminstpath%\jre\bin\java.exe" ^
     -Xmx1024M ^
     -Dceres.context=beam ^
@@ -21,7 +40,7 @@ echo "Running beam to unpack 3dvlab auxdata..."
     "-Dbeam.home=%beaminstpath%" ^
     "-Dncsa.hdf.hdflib.HDFLibrary.hdflib=%beaminstpath%\modules\lib-hdf-2.7\lib\jhdf.dll" ^
     "-Dncsa.hdf.hdf5lib.H5.hdf5lib=%beaminstpath%\modules\lib-hdf-2.7\lib\jhdf5.dll" ^
-    -jar "%beaminstpath%\bin\ceres-launcher.jar" h:\dummy.xml
+    -jar "%beaminstpath%\bin\ceres-launcher.jar" %TEMP%\dummy.xml > nul 2>&1
 
 set helper=%HOMEDRIVE%%HOMEPATH%\.beam\beam-vlab\auxdata\doshelper.jar
 if not exist %helper% (
@@ -29,13 +48,18 @@ if not exist %helper% (
   exit /b 1
 )
 
-echo "Successfully unpacked 3dvlab auxdata"
+echo Successfully unpacked 3dvlab auxdata
 cd /d %HOMEDRIVE%%HOMEPATH%\.beam\beam-vlab\auxdata
 
-rd /q /s dummy_windows32*.*
+echo Removing existing dummy_windows32 (if any)...
+rd /q /s dummy_windows32*.* > nul 2>&1
+echo Fetching binaries for "dummy"...
 "%beaminstpath%"\jre\bin\java -jar %helper%  fetch ftp://ftp.netcetera.ch/pub/dummy_windows32-20130103.zip
+echo Unpacking binaries for "dummy"...
 "%beaminstpath%"\jre\bin\java -jar %helper%  unzip dummy_windows32-20130103.zip
+echo Registering version for "dummy"...
 >> Versions.txt echo dummy_windows32-20130103.zip
+echo Cleaning up...
 rename dummy_windows32-20130103 dummy_windows32
 del dummy_windows32-20130103.zip
 goto :done
